6f863a39ec96a95073fb67fdcee32f31291494b1
Zeeizee <zenizee383@gmail.com>
2021-06-20 13:22:04 -0700

    Projects added

--- js/script.js
+++ js/script.js
@@ -81,7 +81,6 @@ const emailsend=()=>{
         Subject : "GET IN TOUCH (ZEENAT PARVEEN)",
         Body : `Hello ${txtusername.value.trim()}: <br> Thank you for contacting me.I have recieved your email. I will give you response very soon.`
     }).then(
-      message => console.log("abc",message)
     ).catch(err=>{
         console.log(err)
     })
@@ -94,7 +93,7 @@ const emailsend=()=>{
         Subject : `GET IN TOUCH ${txtusername.value}`,
         Body : `Someone trying to connect you through Get In Touch from this Email: ${txtemail.value.trim()} <br><br> ${txtmsg.value.trim()}  `
     }).then(
-      message => console.log("aaa",message)
+      
     ).catch(err=>{
         console.log(err)
     })
